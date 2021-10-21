eda81c10da36ccff2a2fea12849397c308b9264d
Paulo de Freitas <paulohdef1@gmail.com>
2021-10-21 11:49:43 -0300

    first commit]

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,49 @@
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, Output } from "@angular/core";
+import { EventEmitter } from "@angular/core";
+import { Transferencia } from 'src/models/transferencia.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector:'app-nova-transferencia',
+  templateUrl:'./nova-transferencia.component.html',
+  styleUrls:['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private service: TransferenciaService, private router:Router){
+
+  }
+
+  transferir(){
+    console.log('Solicitada nova transferencia');
+
+    const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino};
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) =>{
+        console.log(resultado);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato');
+
+
+      },
+      (error) => console.error(error)
+    );
+    this.limparCampos();
+
+
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+
+  }
+}
---
+++ src/models/transferencia.model.ts
@@ -0,0 +1,8 @@
+
+  export interface Transferencia {
+      id?: string | number;
+      valor: number;
+      destino: string | number;
+      data?: Date;
+  }
+
